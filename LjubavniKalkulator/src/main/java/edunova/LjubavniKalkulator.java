a147a650f09234e48d2f29f7d234da6f5b0ed2a2
isidorakaran <karanisidora@hotmail.com>
2023-02-14 20:07:55 +0100

    Ljubavni kalkulator radi
    Potrebno ispraviti detalje

---
+++ LjubavniKalkulator/src/main/java/edunova/LjubavniKalkulator.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edunova;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author WinUSER
+ */
+public class LjubavniKalkulator {
+
+    public static void main(String[] args) {
+        
+        new Prozor().setVisible(true);
+        
+	}
+    }
+
