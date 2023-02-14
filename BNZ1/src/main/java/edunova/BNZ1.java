9dbaff09a10c944b5581232a7f8b996b5906a9a6
isidorakaran <karanisidora@hotmail.com>
2023-02-14 16:14:02 +0100

    Zadatak BNZ1

---
+++ BNZ1/src/main/java/edunova/BNZ1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edunova;
+
+/**
+ *
+ * @author WinUSER
+ */
+public class BNZ1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new PrviProzor().setVisible(true);
+    }
+}
