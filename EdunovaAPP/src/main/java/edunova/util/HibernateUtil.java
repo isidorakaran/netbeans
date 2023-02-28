7ff93635e21ef1fe525dd242c51f24b436354b08
isidorakaran <karanisidora@hotmail.com>
2023-02-28 23:55:18 +0100

    Napravljena hibernate konfiguracija i klasa HibernateUtil

---
+++ EdunovaAPP/src/main/java/edunova/util/HibernateUtil.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edunova.util;
+
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author WinUSER
+ */
+public class HibernateUtil {
+    
+    private static Session session=null;
+    protected HibernateUtil(){
+        
+    }
+    
+    public static Session getsesSession(){
+        if(session==null){
+            session= new Configuration().configure().buildSessionFactory().openSession();
+        }
+        return session;
+    }
+    
+}
