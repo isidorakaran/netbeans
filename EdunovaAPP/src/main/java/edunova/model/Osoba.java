91319486ff3db6dcea4542f3279f1231a29d1831
isidorakaran <karanisidora@hotmail.com>
2023-02-28 23:55:35 +0100

    Napravljena klasa Osoba

---
+++ EdunovaAPP/src/main/java/edunova/model/Osoba.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edunova.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+public class Osoba {
+    @Id
+    private Integer sifra;
+    private String ime;
+    private String prezime;
+    private BigDecimal primanja;
+    private Boolean aktivan;
+    private Date datumRodenja;
+
+    public Integer getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(Integer sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public BigDecimal getPrimanja() {
+        return primanja;
+    }
+
+    public void setPrimanja(BigDecimal primanja) {
+        this.primanja = primanja;
+    }
+
+    public Boolean getAktivan() {
+        return aktivan;
+    }
+
+    public void setAktivan(Boolean aktivan) {
+        this.aktivan = aktivan;
+    }
+
+    public Date getDatumRodenja() {
+        return datumRodenja;
+    }
+
+    public void setDatumRodenja(Date datumRodenja) {
+        this.datumRodenja = datumRodenja;
+    }
+    
+    
+    
+}
