92050b62a3b122c40c020b5d3a12e96ed32e5c35
isidorakaran <karanisidora@hotmail.com>
2023-02-28 23:58:05 +0100

    Upogonjeno,napravljena tablica Osoba

---
+++ EdunovaAPP/src/main/java/edunova/Start.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edunova;
+
+import edunova.util.HibernateUtil;
+
+/**
+ *
+ * @author WinUSER
+ */
+public class Start {
+
+    public static void main(String[] args) {
+        HibernateUtil.getsesSession();
+    }
+}
