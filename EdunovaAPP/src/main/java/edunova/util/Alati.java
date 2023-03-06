fc6637441d2bddd7f751ed1e08f61f2aed549c73
isidorakaran <karanisidora@hotmail.com>
2023-03-06 12:29:28 +0100

    Metoda za kontrolu OIB-a

--- EdunovaAPP/src/main/java/edunova/util/Alati.java
+++ EdunovaAPP/src/main/java/edunova/util/Alati.java
@@ -71,5 +71,33 @@ public class Alati {
         }
         return true;
     }
+public static boolean kontrolaOib(String oib){
+     if (oib==null || oib.length() != 11) {
+            return false;
+        }
 
+        char[] chars = oib.toCharArray();
+        
+        int a = 10;
+        for (int i = 0; i < 10; i++) {
+        	char c = chars[i];
+        	if (c < '0' || c > '9') {
+        		return false;
+        	}
+            a = a + (c - '0');
+            a = a % 10;
+            if (a == 0) {
+                a = 10;
+            }
+            a *= 2;
+            a = a % 11;
+        }
+        int kontrolni = 11 - a;
+        kontrolni = kontrolni % 10;
+
+        if (kontrolni != (chars[10] - '0')) {
+        	return false;
+        }
+    	return true;
+}
 }
