11a25011a06f722f158dac7225f6f8d6e2d7e4d0
isidorakaran <karanisidora@hotmail.com>
2023-03-06 12:30:06 +0100

    Kontrole rade

--- EdunovaAPP/src/main/java/edunova/Start.java
+++ EdunovaAPP/src/main/java/edunova/Start.java
@@ -4,8 +4,17 @@
 
 package edunova;
 
+import edunova.controller.ObradaGrupa;
+import edunova.controller.ObradaSmjer;
+import edunova.model.Grupa;
+import edunova.model.Polaznik;
+import edunova.model.Smjer;
+import edunova.util.EdunovaException;
+import java.math.BigDecimal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import edunova.util.PocetniInsert;
+
 
 
 /**
@@ -15,6 +24,27 @@ import edunova.util.PocetniInsert;
 public class Start {
 
     public static void main(String[] args) {
-        new PocetniInsert();
+        /*ObradaSmjer os=new ObradaSmjer();
+        Smjer smjer=new Smjer();
+        smjer.setNaziv("Java Programiranje");
+        smjer.setCijena(new BigDecimal(0));
+        os.setEntitet(smjer);
+        try {
+            os.create();
+        } catch (EdunovaException e) {
+            System.out.println(e.getPoruka());
+        }
+        
+        for(Smjer s : os.read()){
+            System.out.println(s.getNaziv());
+        }*/
+        ObradaGrupa og=new ObradaGrupa();
+        for(Grupa g: og.read()){
+            System.out.println(g.getNaziv());
+            System.out.println(g.getPredavac().getIme());
+            for(Polaznik p:g.getPolaznici()){
+                System.out.println("\t" + p.getIme() );
+            }
+        }
     }
 }
